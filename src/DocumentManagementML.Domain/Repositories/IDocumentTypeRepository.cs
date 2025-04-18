a627330a350d2b4fc8746fe18b99650918fa88c4
Marco Santiago <msantiago007@gmail.com>
2025-04-18 19:46:01 +0000

    update the IDocumentTypeRepository.cs interface to align with the implementation we just updated

--- src/DocumentManagementML.Domain/Repositories/IDocumentTypeRepository.cs
+++ src/DocumentManagementML.Domain/Repositories/IDocumentTypeRepository.cs
@@ -18,11 +18,47 @@ namespace DocumentManagementML.Domain.Repositories
         Task<IEnumerable<DocumentType>> GetAllAsync();
         
         /// <summary>
+        /// Gets all document types with pagination
+        /// </summary>
+        /// <param name="skip">Number of document types to skip</param>
+        /// <param name="take">Number of document types to take</param>
+        /// <returns>Paged collection of document types</returns>
+        Task<IEnumerable<DocumentType>> GetAllDocumentTypesAsync(int skip, int take);
+        
+        /// <summary>
+        /// Gets all active document types with pagination
+        /// </summary>
+        /// <param name="skip">Number of document types to skip</param>
+        /// <param name="take">Number of document types to take</param>
+        /// <returns>Paged collection of active document types</returns>
+        Task<IEnumerable<DocumentType>> GetActiveDocumentTypesAsync(int skip, int take);
+        
+        /// <summary>
+        /// Gets all active document types
+        /// </summary>
+        /// <returns>Collection of active document types</returns>
+        Task<IEnumerable<DocumentType>> GetActiveTypesAsync();
+        
+        /// <summary>
         /// Gets a document type by its identifier
         /// </summary>
         /// <param name="id">Document type identifier</param>
         /// <returns>Document type</returns>
         Task<DocumentType?> GetByIdAsync(Guid id);
+        
+        /// <summary>
+        /// Gets a document type by its name
+        /// </summary>
+        /// <param name="name">Document type name</param>
+        /// <returns>Document type if found, null otherwise</returns>
+        Task<DocumentType?> GetByNameAsync(string name);
+        
+        /// <summary>
+        /// Gets the total count of document types
+        /// </summary>
+        /// <param name="activeOnly">If true, count only active document types</param>
+        /// <returns>Total count of document types</returns>
+        Task<int> GetDocumentTypeCountAsync(bool activeOnly = false);
         
         /// <summary>
         /// Adds a new document type
@@ -44,5 +80,12 @@ namespace DocumentManagementML.Domain.Repositories
         /// <param name="id">Document type identifier</param>
         /// <returns>True if the document type was deleted, false otherwise</returns>
         Task<bool> DeleteAsync(Guid id);
+        
+        /// <summary>
+        /// Gets a document type by its identifier including related documents
+        /// </summary>
+        /// <param name="id">Document type identifier</param>
+        /// <returns>Document type with documents if found, null otherwise</returns>
+        Task<DocumentType?> GetWithDocumentsAsync(Guid id);
     }
 }
